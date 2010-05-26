780ae968ded52f7a9aedfe4c7547cc6e3a564074
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-05-26 17:31:31 +0000

    Item6036: updating release id prior to upload
    git-svn-id: http://svn.foswiki.org/trunk/WorkflowPlugin@7563 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/WorkflowPlugin.pm
+++ lib/Foswiki/Plugins/WorkflowPlugin.pm
@@ -20,7 +20,7 @@ use Foswiki::OopsException ();
 use Foswiki::Sandbox ();
 
 our $VERSION          = '$Rev$';
-our $RELEASE          = '03 Feb 2010';
+our $RELEASE          = '26 May 2010';
 our $SHORTDESCRIPTION = 'Supports work flows associated with topics';
 our $NO_PREFS_IN_TOPIC = 1;
 our $pluginName       = 'WorkflowPlugin';
