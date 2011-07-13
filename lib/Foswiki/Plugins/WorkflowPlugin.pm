bf41b8671b1b5f0c8f4290ad021255257b157544
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-07-13 17:24:43 +0000

    Item10963: Put parentheses around qw() in all for(each)? my $i qw( ... ) as it throws a warning on 5.14
    git-svn-id: http://svn.foswiki.org/trunk/WorkflowPlugin@12157 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/WorkflowPlugin.pm
+++ lib/Foswiki/Plugins/WorkflowPlugin.pm
@@ -533,8 +533,8 @@ sub beforeEditHandler {
     # check in this case)
     my $changingState = 1;
     my $query = Foswiki::Func::getCgiQuery();
-    foreach my $p qw(WORKFLOWPENDINGACTION WORKFLOWCURRENTSTATE
-                     WORKFLOWPENDINGSTATE WORKFLOWWORKFLOW) {
+    foreach my $p (qw(WORKFLOWPENDINGACTION WORKFLOWCURRENTSTATE
+                     WORKFLOWPENDINGSTATE WORKFLOWWORKFLOW)) {
         if (!defined $query->param($p)) {
             # All params must be present to change state
             $changingState = 0;
@@ -600,8 +600,8 @@ sub beforeSaveHandler {
     my $query = Foswiki::Func::getCgiQuery();
     my $changingState = 1;
     my %stateChangeInfo;
-    foreach my $p qw(WORKFLOWPENDINGACTION WORKFLOWCURRENTSTATE
-                     WORKFLOWPENDINGSTATE WORKFLOWWORKFLOW) {
+    foreach my $p (qw(WORKFLOWPENDINGACTION WORKFLOWCURRENTSTATE
+                     WORKFLOWPENDINGSTATE WORKFLOWWORKFLOW)) {
         $stateChangeInfo{$p} = $query->param($p);
         if (defined $stateChangeInfo{$p}) {
             $query->delete($p);
