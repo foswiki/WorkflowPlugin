5dff0b5b2a56a4dc1296dba531d54cef92862947
PaulHarvey <PaulHarvey@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-06-13 07:58:50 +0000

    Item11941: Bump revision for release
    git-svn-id: http://svn.foswiki.org/trunk/WorkflowPlugin@14978 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/WorkflowPlugin.pm
+++ lib/Foswiki/Plugins/WorkflowPlugin.pm
@@ -20,7 +20,7 @@ use Foswiki::OopsException                            ();
 use Foswiki::Sandbox                                  ();
 
 our $VERSION = '$Rev$';
-our $RELEASE = '1.12.7';
+our $RELEASE = '1.12.8';
 our $SHORTDESCRIPTION =
 'Associate a "state" with a topic and then control the work flow that the topic progresses through as content is added.';
 our $NO_PREFS_IN_TOPIC = 1;
